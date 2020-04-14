8a784f3fa56c76d5e4a8dfc5d20d58eac59e408e
Vijay Masal <vdmasal@gmail.com>
2020-04-14 17:40:10 +0530

    - Added unit test case in unit testing

--- TestAppTests/TestAppTests.swift
+++ TestAppTests/TestAppTests.swift
@@ -11,24 +11,25 @@ import XCTest
 
 class TestAppTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+   func testSuccessfulInit() {
+        let testSuccessfulJSON: JSON = [
+            "title": "Beavers",
+            "description": "Beavers are second only to humans in their ability to manipulate and change their environment. They can measure up to 1.3 metres long. A group of beavers is called a colony",
+            "imageHref": "http://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/220px-American_Beaver.jpg"
+        ]
+     
+     
+     guard let title = testSuccessfulJSON["firstname"] as? String else {
+         return
+     }
+     
+     guard let description = testSuccessfulJSON["description"] as? String else {
+                return
+            }
+     guard let imageHref = testSuccessfulJSON["imageHref"] as? String else {
+                return
+            }
+     
+        XCTAssertNotNil(Rows(title: title, description: description, imageHref: imageHref))
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
